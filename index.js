4fc9533fb67d979ec4377888e119c7e68764c93d
Richard Braamburg <richard.braamburg@gmail.com>
2026-02-05 11:48:30 +1100

    todo 1 - first route - load custom objects

--- index.js
+++ index.js
@@ -12,8 +12,43 @@ app.use(express.json());
 const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+  const properties = [
+    "game_name",
+    "genre",
+    "release_date",
+    "platform_availability",
+    "rating",
+    "development_status",
+    "base_price",
+    "global_sales",
+    "lead_developer",
+    "game_engine",
+    "store_url",
+  ].join(",");
+  const gamesEndpoint = `https://api.hubapi.com/crm/v3/objects/2-57074073?properties=${encodeURIComponent(properties)}`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const resp = await axios.get(gamesEndpoint, { headers });
+    const data = resp.data.results || [];
+    res.render("games", { title: "Games | HubSpot APIs", data });
+  } catch (error) {
+    console.error("Error fetching games:", error.message);
+    if (error.response) {
+      console.error("HubSpot response status:", error.response.status);
+      console.error(
+        "HubSpot response data:",
+        JSON.stringify(error.response.data, null, 2),
+      );
+    } else {
+      console.error(error);
+    }
+    res.render("games", { title: "Games | HubSpot APIs", data: [] });
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
